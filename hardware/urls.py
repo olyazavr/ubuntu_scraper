8fcbd99517f62237faae1a13da1f5bc4f7b2576d
Olga Shestopalova <olgash@mit.edu>
2014-01-24 13:03:18 -0500

    fix redirect

--- hardware/urls.py
+++ hardware/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^', redirect_to, {'url': '/search/'}),
+	url(r'^', include('scraper.urls')),
     url(r'^search/', include('scraper.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
