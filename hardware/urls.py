72ccbbaeb23c204e325e0c11af85acee395f7d85
Olga Shestopalova <olgash@mit.edu>
2014-01-24 12:59:30 -0500

    oops forgot a homepage

--- hardware/urls.py
+++ hardware/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^', redirect_to, {'url': '/search/'}),
     url(r'^search/', include('scraper.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
